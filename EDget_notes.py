bf2e2fa46d04767b33abf1df1ad83ca280064640
Lun4rIum <67378940+Lun4rIum@users.noreply.github.com>
2022-03-03 16:28:01 +0100

    Update EDget_notes.py

--- EDget_notes.py
+++ EDget_notes.py
@@ -75,3 +75,31 @@ def get_notes(periode,identifiant,mdp):
             Short_note=infoNote["codePeriode"].replace("A001","1").replace("A002","2").replace("A003","3"),infoNote["libelleMatiere"],float(float(Short_note[2])*20/float(Short_note[3]))
 
             Data[Short_note[1]].append(str(Short_note[2]).replace(".0",""))
+
+async def DiscordMessageNotes(client,message):            
+    if message.content.startswith("!notes"):
+            try:
+                if isinstance(message.channel, discord.DMChannel):
+
+
+                    commande = message.content.split(' ')
+                    trimestre = int(commande[1])
+                    identifiant=commande[2]
+                    mdp = commande[3]
+                    get_notes(trimestre,identifiant,mdp)
+
+                    embed=discord.Embed(title=f"__Notes du {trimestre} trimestre : __", description="", color=discord.Color.from_rgb(111, 250, 100))
+                    embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+                    embed.set_footer(text=f"Donne toi à fond {message.author.name} ! ❤️")
+                    
+                    for k, v in Data.items():
+
+                        embed.add_field(name=k, value='\n'.join(v), inline=True)
+                    await message.channel.send(embed=embed)
+                else:
+                    await message.delete()
+                    await message.channel.send("Merci d'utiliser cette commande en privée pour des raisons de sécurité")
+                    await message.author.send("Veuillez réessayez ici ! ")
+            except Exception as err:
+                
+                await message.channel.send("Une erreur est survenue ! (mauvais identifiants ?, syntaxe incorrect) essayez ! aide")
