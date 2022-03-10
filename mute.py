e08f5f0ca2965a85ca952a04c787c8c8be412843
BiMathAx <61063210+MathKode@users.noreply.github.com>
2022-03-10 11:18:01 +0100

    Delete mute.py

--- mute.py
+++
@@ -1,8 +0,0 @@
-import discord
-
-async def __add_role(message,member,role_name):
-    print(message.guild.roles)
-    #discord.utils.get(message.guild.roles)
-    #await member.add_roles()
-async def mute(client,message):
-    await __add_role(message,message.author,"mute")
