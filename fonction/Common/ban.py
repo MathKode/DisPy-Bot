b6efc45f30382f96162f55da091b1ed76cd67c11
Lun4rIum <67378940+Lun4rIum@users.noreply.github.com>
2022-03-13 10:08:50 +0100

    Update ban.py

--- fonction/Common/ban.py
+++ fonction/Common/ban.py
@@ -13,8 +13,6 @@ async def ban(client, message):
     membre=commande[1]
   
     try:
-      if len(commande)==1:
-        await message.channel.send("Tu n'as pas la préciser de **membre** à bannir pour executer cette commande")
       num = int(''.join(re.findall(r'\d+', membre)))
 
       user = await message.guild.query_members(user_ids=[num]) 
@@ -27,13 +25,23 @@ async def ban(client, message):
       embed.set_author(name=user.name, icon_url=user.avatar_url)
 
       if len(commande)==2:
+        reason_ = "Aucune raison fournie"
         embed.add_field(name="Informations : ",value=f"banni de **{user.guild}**,\nRaison : Aucune raison fournie", inline=True)
         await user.send(embed=embed)
         await user.ban(delete_message_days=7)
       elif len(commande)>2:
+        reason_ = f"{' '.join(commande[2:])}"
         embed.add_field(name="Informations : ",value=f"banni de **{user.guild}**,\nRaison : {' '.join(commande[2:])}", inline=True)
         await user.send(embed=embed)
         await user.ban(reason=' '.join(commande[2:]), delete_message_days=7)
+
+
+      validation_embed=discord.Embed(title=f"__ {user} a été banni ! __", description="", color=discord.Color.from_rgb(42, 255, 0))
+      validation_embed.add_field(name="Raison : ",value=reason_, inline=True)
+      validation_embed.set_author(name=user.name, icon_url=user.avatar_url)
+      validation_embed.set_footer(text=f"Expulsé par {message.author.name} !")
+
+      await message.channel.send(embed=validation_embed)
 
       
       print(f"{user} as been banned")
