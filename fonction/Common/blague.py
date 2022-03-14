a23e9817476f92fba6b504d8af4f44d7725f8d53
Lun4rIum <67378940+Lun4rIum@users.noreply.github.com>
2022-03-14 22:12:18 +0100

    Update blague.py

--- fonction/Common/blague.py
+++ fonction/Common/blague.py
@@ -3,20 +3,40 @@ import discord
 
 
 
-
 async def joke(client,message):
-    try:
-        blagues = BlaguesAPI("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNDUxNzgxNjM3OTQwOTY5NDczIiwibGltaXQiOjEwMCwia2V5IjoiY2hVSWV2R1N5TGhqTkpOQk5UY2pGd0ROcDZ6T3ZRazlhUE84MjZlTFdBTGlxUVNiM1kiLCJjcmVhdGVkX2F0IjoiMjAyMi0wMy0xNFQyMDowMToxNyswMDowMCIsImlhdCI6MTY0NzI4ODA3N30.xvOy4BxYn0hF9Q8h3lYSqn5dXnja1-HbY12M0x91-W4")
-        Blague = await blagues.random()
+    commande = message.content.split(' ')
+    if len(commande) == 1:
+    
 
-        embed=discord.Embed(title=f"__ Et voici pour vous une blague de la meilleure des qualités ! __", description="", color=discord.Color.from_rgb(51, 218, 255))
-        embed.set_footer(text=f"Si c'est une blague raciste, nous ne sommes pas responsables !")
-        embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+        try:
+            blagues = BlaguesAPI("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNDUxNzgxNjM3OTQwOTY5NDczIiwibGltaXQiOjEwMCwia2V5IjoiY2hVSWV2R1N5TGhqTkpOQk5UY2pGd0ROcDZ6T3ZRazlhUE84MjZlTFdBTGlxUVNiM1kiLCJjcmVhdGVkX2F0IjoiMjAyMi0wMy0xNFQyMDowMToxNyswMDowMCIsImlhdCI6MTY0NzI4ODA3N30.xvOy4BxYn0hF9Q8h3lYSqn5dXnja1-HbY12M0x91-W4")
+            Blague = await blagues.random()
+
+            embed=discord.Embed(title=f"__ Et voici pour vous une blague de la meilleure des qualités ! __", description="", color=discord.Color.from_rgb(51, 218, 255))
+            embed.set_footer(text=f"Si c'est une blague raciste, nous ne sommes pas responsables !")
+            embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
 
 
-        embed.add_field(name=f"{Blague.joke}",value=f"{Blague.answer}", inline=False)
-        await message.channel.send(embed=embed)
+            embed.add_field(name=f"{Blague.joke}",value=f"{Blague.answer}", inline=False)
+            await message.channel.send(embed=embed)
+            
+        except Exception as err:
+            print(err)
         
-    except Exception as err:
-        print(err)
-        
+    else:
+        JokeType = commande[1]
+        if JokeType == "dark":
+            try:
+                blagues = BlaguesAPI("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNDUxNzgxNjM3OTQwOTY5NDczIiwibGltaXQiOjEwMCwia2V5IjoiY2hVSWV2R1N5TGhqTkpOQk5UY2pGd0ROcDZ6T3ZRazlhUE84MjZlTFdBTGlxUVNiM1kiLCJjcmVhdGVkX2F0IjoiMjAyMi0wMy0xNFQyMDowMToxNyswMDowMCIsImlhdCI6MTY0NzI4ODA3N30.xvOy4BxYn0hF9Q8h3lYSqn5dXnja1-HbY12M0x91-W4")
+                Blague = await blagues.random_categorized('dark')
+
+                embed=discord.Embed(title=f"__ ATTENTION HUMOUR NOIR ! __", description="", color=discord.Color.from_rgb(51, 218, 255))
+                embed.set_footer(text=f"Si c'est une blague raciste, nous ne sommes pas responsables !")
+                embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+
+
+                embed.add_field(name=f"{Blague.joke}",value=f"|| {Blague.answer} ||", inline=False)
+                await message.channel.send(embed=embed)
+                
+            except Exception as err:
+                print(err)
